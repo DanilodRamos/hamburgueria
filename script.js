a5fb5e3a28615331e4c67497f0e77d2144dd7941
DanilodeRamos <Danilodramos@proton.me>
2024-07-19 14:46:27 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -96,5 +96,91 @@ function updateCartModal(){
 cartItemsContainer.addEventListener("click", function (event){
     if(event.target.classList.container("remove-from-cart-btn")){
         const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+
     }
 })
+function removeItemCart(name){
+    const  index = cart.findIndex(item => item.name === name);
+    if(index !== -1){
+        const item = cart[index];
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+//finalizar pedido
+checkoutBtn.addEventListener("click", function(){
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+       Toastify({
+        text: "Ops  o restaurante está fechado",
+        duration: 3000,
+        close: true,
+        gravity:"top",
+        position: "rigth",
+        stopOnFocus: true,
+        style:{
+            background:"#ef4444",
+        },
+       }).showToast();
+       return;
+    }
+
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+    
+//enviar pedido whatzap
+ const cartItems = cart.map((item) => {
+    return(
+        ` ${item.name}Quantidade: (${item.quantity}) Preço: R$${item.price}`
+    )
+    
+ }).join("")
+ const message = encodeURIComponent(cartItems)
+ const phone = "34988064751"
+
+ window.open(`https://wa.me/${phone}?text=${
+    message} Endereço: ${addressInput.value}`, "_blank")
+    cart.length = [];
+    updateCartModal();
+})
+
+
+ //vereficar a hora  e manipular o card horario
+function checkRestaurantOpen(){
+    const data= new Date();
+    const hora = data.getHours();
+    return hora >= 18 &&  hora < 22;
+ }
+
+const spanItem = document.getElementById("date-span")
+const isOpen =  checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
